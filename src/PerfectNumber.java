48bf49896330347b36296d8fc73d4175b896f884
Mikhail <mikerar329@gmail.com>
2019-02-21 00:31:00 +0300

    Perfect check[2]

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -1,8 +1,9 @@
+import java.lang.ref.SoftReference;
 import java.util.Scanner;
 
 public class PerfectNumber {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         if (isPerfect(n)) {
