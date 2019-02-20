f438ca9d3d468a97b0b092bc82a947183a4621af
Mikhail <mikerar329@gmail.com>
2019-02-20 23:38:22 +0300

    Perfect check

---
+++ src/PerfectNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (isPerfect(n)) {
+            System.out.println("Perfect");
+        } else {
+            System.out.println("Not perfect");
+        }
+    }
+
+    private static boolean isPerfect(int a) {
+        int sum = 0;
+        for (int i = 1; i < a; i++)
+            if (a % i == 0) sum += i;
+        if (a == sum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
