eaf23e51c6239a89cdee5ac33cf45d0e7e8748da
Mikhail <mikerar329@gmail.com>
2019-02-27 13:41:57 +0300

    changes

--- src/PrimeDigit.java
+++ src/PrimeDigit.java
@@ -5,12 +5,12 @@ public class PrimeDigit {
     public static void main(String args[]) {
         int i = 2;
         boolean isPrime = true;
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter number1:");
-
         int num = scan.nextInt();
 
-        while (isPrime && i < (num / 2 + 1)) {
+        while (isPrime && i <= Math.sqrt(num)) {
 
             if (num % i == 0) {
                 isPrime = false;
