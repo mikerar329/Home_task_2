89f11399d315d0b2a0594430cafcfbd6c71d68f4
Mikhail <mikerar329@gmail.com>
2019-02-27 12:27:51 +0300

    changes

--- src/HeadOrTail.java
+++ src/HeadOrTail.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class HeadOrTail {
 
     public static void main(String[] args) {
         int head = 0;
+        Random rnd = new Random();
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number1 of attempts:");
         int n = input.nextInt();
         for (int i = 0; i < n; i++) {
-            if (Math.random() < 0.5) {
+            if (rnd.nextBoolean()) {
                 head++;
             }
         }
